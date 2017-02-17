a88ea16664db625d0117ae55f6d7152c10431988
student <student@K0606>
2017-02-17 16:50:16 +0200

    add class Dyadchenko

---
+++ ConsoleApplication1/Dyadchenko/Dyadchenko.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1.Box;
+using ConsoleApplication1.Car;
+using ConsoleApplication1.Size;
+
+namespace ConsoleApplication1.Dyadchenko
+{
+    public static class Dyadchenko
+    {
+        public static double DyadchenkoMethod()
+        {
+            List<IBox> boxes = new List<IBox>
+            {
+                new BoxMetal(),
+                new BoxMetal(),
+                new BoxMetal()
+            };
+
+            var pakege = new BoxOfBoxes(boxes);
+
+            ICar car = new SmallCar();
+            return car.CountPrice(boxes);
+        }
+    }
+}
